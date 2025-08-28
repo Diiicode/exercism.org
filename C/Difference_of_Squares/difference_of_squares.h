e4d33c338a6de1c977df5c0e8a1d19c6a73eae07
Di Code <di-code@outlook.com>
2025-08-28 16:57:10 +0100

    Add files via upload

---
+++ C/Difference_of_Squares/difference_of_squares.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   difference_of_squares.h                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 16:54:58 by diserra           #+#    #+#             */
+/*   Updated: 2025/08/28 16:55:50 by diserra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DIFFERENCE_OF_SQUARES_H
+#define DIFFERENCE_OF_SQUARES_H
+
+unsigned int sum_of_squares(unsigned int number);
+unsigned int square_of_sum(unsigned int number);
+unsigned int difference_of_squares(unsigned int number);
+
+#endif
